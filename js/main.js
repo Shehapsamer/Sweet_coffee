5781119532531feef3a56b418661ce00b9d103fb
‪shehap Samer‬‏ <118848577+Shehapsamer@users.noreply.github.com>
2024-01-06 17:28:08 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// /////////////////////
+window.addEventListener("scroll", function () {
+    let nav = document.querySelector(".navbar");
+    nav.classList.toggle("fix", window.scrollY > 150);
+}) 
+//////////////////////////
+window.addEventListener("scroll" , function () {
+    let scrollUp = document.querySelector(".scroll_up");
+    scrollUp.classList.toggle("scroll_transition", window.scrollY > 150);
+    scrollUp.classList.toggle("show_scroll", window.scrollY > 200)
+})
+    let scrollUp = document.querySelector(".scroll_up");
+
+scrollUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior:"smooth"
+    })
+})
